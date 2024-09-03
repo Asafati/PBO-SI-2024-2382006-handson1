e49554f4d442b77b27ec48423909316c0a30898a
Asafati Gea <2382006@unai.edu>
2024-09-03 11:01:45 +0700

    Handson1

---
+++ TipeDataCharacterDanBoolean.java
@@ -0,0 +1,10 @@
+public class TipeDataCharacterDanBoolean {
+}public static void main(String[]nargs) {
+    short tipeDataShort = 10;
+    int tipeDataInt = DataShort ;
+    long tipeDataLong = tipeDataInt;
+
+    int tipeDataInteger2 = 10;
+    byte tipeDataByte = (byte) tipeDataInteger2;
+    ;
+}
